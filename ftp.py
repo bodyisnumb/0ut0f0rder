ff55cb7b5748deb97f7083260984802a4c80b2d8
bodyisnumb <goblimgus@gmail.com>
2018-11-08 15:37:18 +0200

    GridFS function 1.1

--- ftp.py
+++ ftp.py
@@ -1,4 +1,7 @@
+from pymongo import MongoClient
 import ftplib
+import gridfs
+
 
 
 #  Connect and login to the FTP server.
@@ -7,38 +10,58 @@ ftp.login(user='pi', passwd='feedus321')
 ftp.cwd('/files')
 
 
-# Create list of photos
-files = []
+def get_list():
+    # Create list of photos
+    files = []
 
 # Print list of files in remote directory
-try:
-    files = ftp.nlst()
-except ftplib.error_perm as resp:
-    if str(resp) == "550 No files found":
-        print("No files in this directory")
+    try:
+        files = ftp.nlst()
+    except ftplib.error_perm as resp:
+        if str(resp) == "550 No files found":
+            print("No files in this directory")
+        else:
+            raise
+
+    for f in files:
+        print(f)
+
+
+def size_check():
+    # Check if file exists
+    ftp.cwd('/home/pi/ftp/files')
+    ftp.sendcmd("TYPE i")
+    file_size = ftp.size("photo.jpg")
+    if file_size < 0:
+        print("file does not exist")
     else:
-        raise
-
-for f in files:
-    print(f)
-
-# Check if file exists
-ftp.sendcmd("TYPE i")
-file_size = ftp.size("/home/pi/ftp/files/photo.jpg")
-if file_size < 0:
-    print("file does not exist")
-else:
-    print("file exists and is " + str(file_size) + " bytes in size")
+        print("file exists and is " + str(file_size) + " bytes in size")
 
 
-# Get file from the remote directory
-file_name = "photo.jpg"
-local_file = open(file_name, 'rb')
-ftp.retrbinary('RETR' + file_name, local_file.write, 1024)
+def grab_file():
+    # Get file from the remote directory
+    ftp.cwd('/home/pi/ftp/files')
+    file_name = "photo.jpg"
+    local_file = open(file_name, 'rb')
+    ftp.retrbinary('RETR' + file_name, local_file.write, 1024)
+    ftp.quit()
+    local_file.close()
 
-ftp.quit()
 
+#def place_file():
+#    filename = 'photo.jpg'
+#    ftp.storbinary('STOR '+filename, open(filename, 'rb'))
+#    ftp.quit()
 
+def load_to_db_by_name():
+    my_db = MongoClient().test
+    fs = GridFSBucket(my_db)
+    # Get file to write to
+    file = open('photo.jpg','wb')
+    fs.load_to_db_by_name("photo.jpg", file)
 
-
-
+if __name__ == "__main__":
+    get_list()
+    size_check()
+    grab_file()
+    place_file()
