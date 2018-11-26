080413f1bbe5923ee0a3174880b89028444b98ef
bodyisnumb <goblimgus@gmail.com>
2018-11-26 22:21:11 +0200

    ftp fixes

--- ftp.py
+++ ftp.py
@@ -3,14 +3,19 @@ import ftplib
 import gridfs
 
 
+def refresh():
+    #  Connect and login to the FTP server.
+    ftp = ftplib.FTP("192.168.0.127")
+    ftp.login(user='pi', passwd='feedus321')
+    ftp.cwd('/files')
+    for filename in get_list(ftp):
+        if size_check(filename, ftp):
+            fp = grab_file(ftp)
+            load_to_db_by_name(fp)
+            delete_file(ftp, filename)    
 
-#  Connect and login to the FTP server.
-ftp = ftplib.FTP("192.168.0.127")
-ftp.login(user='pi', passwd='feedus321')
-ftp.cwd('/files')
 
-
-def get_list():
+def get_list(ftp):
     # Create list of photos
     files = []
 
@@ -19,50 +24,43 @@ def get_list():
         files = ftp.nlst()
     except ftplib.error_perm as resp:
         if str(resp) == "550 No files found":
-            print("No files in this directory")
+            return tuple()
         else:
             raise
 
-    for f in files:
-        print(f)
+    for line in files:
+        print(line)
 
 
-def size_check():
+def size_check(filename, ftp):
     # Check if file exists
     ftp.cwd('/home/pi/ftp/files')
     ftp.sendcmd("TYPE i")
-    file_size = ftp.size("photo.jpg")
+    file_size = ftp.size(filename)
     if file_size < 0:
         print("file does not exist")
     else:
         print("file exists and is " + str(file_size) + " bytes in size")
 
 
-def grab_file():
+def grab_file(ftp):
     # Get file from the remote directory
     ftp.cwd('/home/pi/ftp/files')
-    file_name = "photo.jpg"
-    local_file = open(file_name, 'rb')
-    ftp.retrbinary('RETR' + file_name, local_file.write, 1024)
-    ftp.quit()
-    local_file.close()
+    filename = "photo.jpg"
+    local_file = open(filename, 'rb')
+    ftp.retrbinary('RETR' + filename, local_file.write, 1024)
+    return filename
+ #   ftp.quit()
+ #   local_file.close()
 
 
-#def place_file():
-#    filename = 'photo.jpg'
-#    ftp.storbinary('STOR '+filename, open(filename, 'rb'))
-#    ftp.quit()
-
-def load_to_db_by_name():
+def load_to_db_by_name(fp):
     my_db = MongoClient().test
-    fs = GridFSBucket(my_db)
+    fs = gridfs.GridFS(my_db)
     # Get file to write to
-    file = open('photo.jpg','wb')
-    fs.load_to_db_by_name("photo.jpg", file)
+    fs.put(fp)
 
-if __name__ == "__main__":
-    get_list()
-    size_check()
-    grab_file()
-#    place_file()
-    load_to_db_by_name()
+
+def delete_file(ftp, filename):
+    ftp.delete(filename)
+
